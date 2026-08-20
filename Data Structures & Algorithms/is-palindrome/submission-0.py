7862f6d92368393bafe5a9695df246c76ffa4979
Christian Hower <100043321+ChristianHower@users.noreply.github.com>
2026-08-20 14:29:56 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        noSpace = s.lower()
+        start = 0
+        end = len(noSpace) - start - 1
+        while start < end:
+            if (noSpace[start] == noSpace[end]):
+                start += 1
+                end -= 1
+            elif (noSpace[start].isalnum() == False):
+                start += 1
+            elif (noSpace[end].isalnum() == False):
+                end -= 1
+            else:
+                print(noSpace[start])
+                print(noSpace[end])
+                return False
+        return True
