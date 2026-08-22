2ffd774c5664824a24f45167ab5a885a8d466e61
Christian Hower <100043321+ChristianHower@users.noreply.github.com>
2026-08-22 18:57:53 -0400

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # put list in order
+        nums.sort()
+
+        # create hashmap of all nums with their counts
+        hash = {}
+        count = 0
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i-1] != nums[i]:
+                hash[nums[i-1]] = count
+                count = 0
+            count += 1
+        if nums:
+            hash[nums[-1]] = count
+
+        # k times, add hashmap key with greatest value, then remove the pair from og hashmap
+        final =[]
+        for j in range(k):
+            top = max(hash, key=hash.get)
+            final.append(top)
+            hash.pop(top)
+
+        # return final list
+        return(final)
